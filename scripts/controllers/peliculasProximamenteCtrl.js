10b39b5d4393c0ee7b13d3ed8fa90bf20ada0c14
Eugenio Mendoza <eu.mendoza@gmail.com>
2015-09-23 17:17:06 +0200

    Movemos el filtro al controlador

--- scripts/controllers/peliculasProximamenteCtrl.js
+++ scripts/controllers/peliculasProximamenteCtrl.js
@@ -1,9 +1,9 @@
-angular.module('myFilmsApp').controller('peliculasProximamenteCtrl', ['$scope', 'ApiService', function($scope, ApiService) {
+angular.module('myFilmsApp').controller('peliculasProximamenteCtrl', ['$scope', 'ApiService','$filter', function($scope, ApiService,$filter) {
 	ApiService
 		.consultaApi('movie/upcoming')
 		.then(
 			function(response) {
-				$scope.peliculas = response.data.results;
+				$scope.peliculas = $filter('orderBy')(response.data.results,['release_date','title']);
 			},
 			function() {
 				console.log('No se ha podido recopilar la información');
